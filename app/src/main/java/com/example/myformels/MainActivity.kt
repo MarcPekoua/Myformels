41d53ff26e32608149c51169a2399f6491375a8a
MarcPekoua <marcpekoua@gmail.com>
2020-11-04 23:09:43 +0100

    navigation and first page

--- app/src/main/java/com/example/myformels/MainActivity.kt
+++ app/src/main/java/com/example/myformels/MainActivity.kt
@@ -2,10 +2,25 @@ package com.example.myformels
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import androidx.appcompat.app.ActionBarDrawerToggle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    lateinit var toggle: ActionBarDrawerToggle
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        toggle = ActionBarDrawerToggle(this, firstView, R.string.open, R.string.close)
+        firstView.addDrawerListener(toggle)
+        toggle.syncState()
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if(toggle.onOptionsItemSelected(item))
+            return true
+         return super.onOptionsItemSelected(item)
     }
 }
