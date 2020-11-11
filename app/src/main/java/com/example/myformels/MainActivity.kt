91c2f44e6ff0b2f218d899ae864bf56567603a03
MarcPekoua <marcpekoua@gmail.com>
2020-11-11 22:04:32 +0100

    search bar test

--- app/src/main/java/com/example/myformels/MainActivity.kt
+++ app/src/main/java/com/example/myformels/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.myformels
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.MenuItem
+import android.widget.Toast
 import androidx.appcompat.app.ActionBarDrawerToggle
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -16,7 +17,20 @@ class MainActivity : AppCompatActivity() {
         firstView.addDrawerListener(toggle)
         toggle.syncState()
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        navView.setNavigationItemSelectedListener {
+
+          when(it.itemId){
+              R.id.Fach1 -> Toast.makeText(applicationContext,"clicked Math",Toast.LENGTH_LONG).show()
+              R.id.Fach2 -> Toast.makeText(applicationContext,"clicked Phy",Toast.LENGTH_LONG).show()
+              R.id.Fach3 -> Toast.makeText(applicationContext,"clicked Chemie",Toast.LENGTH_LONG).show()
+              R.id.Fach4 -> Toast.makeText(applicationContext,"clicked Elektrotechnik",Toast.LENGTH_LONG).show()
+
+          }
+            true
+        }
+        //TODO("implement the real pages")
     }
+
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         if(toggle.onOptionsItemSelected(item))
