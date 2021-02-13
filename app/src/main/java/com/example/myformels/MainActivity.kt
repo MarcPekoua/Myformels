84386f9714d2e02221b9e4988804e34853b2b77f
Omer Guimdo <omersucces@yahoo.com>
2021-02-13 09:41:47 +0100

    design main xml

--- app/src/main/java/com/example/myformels/MainActivity.kt
+++ app/src/main/java/com/example/myformels/MainActivity.kt
@@ -14,6 +14,7 @@ import android.widget.Toast
 import android.widget.Toolbar
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.widget.ToolbarWidgetWrapper
+import androidx.core.content.ContextCompat.startActivity
 import androidx.room.Room
 import kotlinx.android.synthetic.*
 import kotlinx.android.synthetic.main.activity_main.*
