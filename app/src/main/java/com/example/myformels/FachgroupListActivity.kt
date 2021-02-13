2e22d82e7d127281fb4c331d0e3ca9f09e59d209
Omer Guimdo <omersucces@yahoo.com>
2021-02-13 21:39:49 +0100

    ScrollView und parentActivity to FormelListActivity

--- app/src/main/java/com/example/myformels/FachgroupListActivity.kt
+++ app/src/main/java/com/example/myformels/FachgroupListActivity.kt
@@ -69,6 +69,7 @@ class FachgroupListActivity() : AppCompatActivity(), OnGroupClickListener {
     }
 
     override fun onGroupItemClicked(position: Int) {
-        Toast.makeText(this ,data!![position].name, Toast.LENGTH_SHORT).show()
+
+        startActivity(Intent(this,FormelListActivity::class.java))
     }
 }
